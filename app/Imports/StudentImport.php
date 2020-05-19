f57c2604729f95a126eaed2dd23d3bd8c30c93ee
danielzalukhu <33409087+danielzalukhu@users.noreply.github.com>
2020-05-19 21:38:05 +0700

    add user store at StudentImport to create user login after create siswa

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -4,8 +4,9 @@ namespace App\Imports;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Illuminate\Support\Facades\Hash;
 use App\Student;
-use Hash;
+use App\User;
 use DB;
 
 class StudentImport implements ToCollection
@@ -29,18 +30,30 @@ class StudentImport implements ToCollection
                 {
                     $id_kelas = $ig->id;
                 }
-                // dd($id_kelas);
+                
                 if($key >= 5)
                 {
+                    $student_full_name = $row[5];
                     $studentName = explode(" ", $row[5], 2);
 
-                    Student::create([
+                    $siswa = Student::create([
                         'NISN' => $row[4],
                         'FNAME' => $studentName[0],
                         'LNAME' => $studentName[1],
                         'GRADES_ID' => $id_kelas
                     ]); 
+
+                    $user = User::create([
+                        'name' => $student_full_name,
+                        'email' => $studentName[0] . "@student.ac.id",
+                        'password' => Hash::make($studentName[0]),
+                        'ROLE' => 'STUDENT',
+                        'STUDENTS_ID' => $siswa->id
+                                             
+                    ]);
                 }   
+
+                
             }            
         }    
     }
