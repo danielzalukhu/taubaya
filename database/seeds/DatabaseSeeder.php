8724ed38a6d797e4e3c532a5ed486be493802096
danielzalukhu <33409087+danielzalukhu@users.noreply.github.com>
2020-08-06 22:49:46 +0700

    re-install cms

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call(TokenSeeder::class);
         // $this->call(StudentSeeder::class);
         // $this->call(GradeStudentSeeder::class);
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(ViolationSeeder::class);
         $this->call(AchievementSeeder::class);
         $this->call(SubjectSeeder::class);
